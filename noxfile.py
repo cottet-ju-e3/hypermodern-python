4fee7415881a5fb081a665da3d82e704b1c56961
cottet julien <julien.cottet@group.leguide.com>
2020-05-10 17:32:25 +0200

    add nox dev libraries

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
-import nox
-
 import tempfile
+
+import nox
 
 nox.options.sessions = "black", "lint", "safety", "tests"
 locations = "src", "tests", "noxfile.py"
@@ -9,7 +9,10 @@ locations = "src", "tests", "noxfile.py"
 @nox.session(python=["3.8", "3.7"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
-    session.run("poetry", "install", external=True)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
+    )
     session.run("pytest", *args)
 
 
@@ -19,7 +22,8 @@ locations = "src", "tests", "noxfile.py"
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install(
+    install_with_constraints(
+        session,
         "flake8",
         "flake8-bandit",
         "flake8-black",
@@ -29,11 +33,10 @@ def lint(session):
     session.run("flake8", *args)
 
 
-# noxfile.py
 @nox.session(python="3.8")
 def black(session):
     args = session.posargs or locations
-    session.install("black")
+    install_with_constraints(session, "black")
     session.run("black", *args)
 
 
@@ -49,5 +52,18 @@ def safety(session):
             f"--output={requirements.name}",
             external=True,
         )
-        session.install("safety")
+        install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
