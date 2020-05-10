6e55808a0f69daf113d345338881b6afb7d0c881
cottet julien <julien.cottet@group.leguide.com>
2020-05-10 17:13:10 +0200

    add nox safety

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,8 @@
 import nox
 
+import tempfile
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "black", "lint", "safety", "tests"
 locations = "src", "tests", "noxfile.py"
 
 
@@ -18,7 +19,13 @@ locations = "src", "tests", "noxfile.py"
 @nox.session(python=["3.8", "3.7"])
 def lint(session):
     args = session.posargs or locations
-    session.install("flake8", "flake8-black", "flake8-import-order")
+    session.install(
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
     session.run("flake8", *args)
 
 
@@ -28,3 +35,19 @@ def black(session):
     args = session.posargs or locations
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
